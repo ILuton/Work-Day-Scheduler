434b3456b7b2e80fcacecfe1b9963f5eb6d532eb
Isaac Luton <isaacdluton@gmail.com>
2022-03-26 20:03:51 -0700

    update to have time blocks change colors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //input boxes
-const nineTime = document.getElementById("nineTime");
+let nineTime = document.getElementById("nineTime");
 const tenTime = document.getElementById("tenTime");
 const elevenTime = document.getElementById("elevenTime");
 const noonTime = document.getElementById("noonTime");
@@ -83,12 +83,115 @@ function getTask () {
 
 function dayAndTime () {
 
-    currentDay.textContent = moment(); 
+    currentDay.textContent = moment().format("MMMM Do YYYY, h:mm:ss"); 
 
+    setInterval(dayAndTime, 1000);
+}
+
+//adding hour values to each timeblock and checking if current time is before or after
+
+function timeCheck() {
+
+    currentHour = moment().format("HH")
+    
+    
+    let nineTimestamp = ("09");
+
+    if (nineTimestamp === currentHour) {
+        nineTime.style.backgroundColor = ("#d3d3d3");
+    } else if (nineTimestamp < currentHour){
+        nineTime.style.backgroundColor = ("#ff6961");
+    } else {
+        nineTime.style.backgroundColor = ("#77dd77");
+    };
+
+    let tenTimestamp = ("10");
+
+    if (tenTimestamp === currentHour) {
+        tenTime.style.backgroundColor = ("#d3d3d3");
+    } else if (tenTimestamp < currentHour){
+        tenTime.style.backgroundColor = ("#ff6961");
+    } else {
+        tenTime.style.backgroundColor = ("#77dd77");
+    };
+
+    let elevenTimestamp = ("11");
+
+    if (elevenTimestamp === currentHour) {
+        elevenTime.style.backgroundColor = ("#d3d3d3");
+    } else if (elevenTimestamp < currentHour){
+        elevenTime.style.backgroundColor = ("#ff6961");
+    } else {
+        elevenTime.style.backgroundColor = ("#77dd77");
+    };
+
+    let noonTimestamp = ("12");
+
+    if (noonTimestamp === currentHour) {
+        noonTime.style.backgroundColor = ("#d3d3d3");
+    } else if (noonTimestamp < currentHour){
+        noonTime.style.backgroundColor = ("#ff6961");
+    } else {
+        noonTime.style.backgroundColor = ("#77dd77");
+    };
+
+    let oneTimestamp = ("13");
+
+    if (oneTimestamp === currentHour) {
+        oneTime.style.backgroundColor = ("#d3d3d3");
+    } else if (oneTimestamp < currentHour){
+        oneTime.style.backgroundColor = ("#ff6961");
+    } else {
+        oneTime.style.backgroundColor = ("#77dd77");
+    };
+
+    let twoTimestamp = ("14");
+
+    if (twoTimestamp === currentHour) {
+        twoTime.style.backgroundColor = ("#d3d3d3");
+    } else if (twoTimestamp < currentHour){
+        twoTime.style.backgroundColor = ("#ff6961");
+    } else {
+        twoTime.style.backgroundColor = ("#77dd77");
+    };
+
+    let threeTimestamp = ("15");
+
+    if (threeTimestamp === currentHour) {
+        threeTime.style.backgroundColor = ("#d3d3d3");
+    } else if (threeTimestamp < currentHour){
+        threeTime.style.backgroundColor = ("#ff6961");
+    } else {
+        threeTime.style.backgroundColor = ("#77dd77");
+    };
+
+    let fourTimestamp = ("16");
+
+    if (fourTimestamp === currentHour) {
+        fourTime.style.backgroundColor = ("#d3d3d3");
+    } else if (fourTimestamp < currentHour){
+        fourTime.style.backgroundColor = ("#ff6961");
+    } else {
+        fourTime.style.backgroundColor = ("#77dd77");
+    };
+
+    let fiveTimestamp = ("17");
+
+    if (fiveTimestamp === currentHour) {
+        fiveTime.style.backgroundColor = ("#d3d3d3");
+    } else if (fiveTimestamp < currentHour){
+        fiveTime.style.backgroundColor = ("#ff6961");
+    } else {
+        fiveTime.style.backgroundColor = ("#77dd77");
+    };
 
 }
-// getTask();
-// saveTask();
-dayAndTime();
 
-console.log(moment());
+function init() {
+    getTask();
+    setTask();
+    dayAndTime();
+    timeCheck();
+}
+
+init();
